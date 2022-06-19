161d53abe81da2848c24a99b6044dd6cf38f96ea
Rafael Cardoso <>
2022-06-19 22:05:35 +1000

    Fixed colors table

--- src/ui/components/table/DataTable.tsx
+++ src/ui/components/table/DataTable.tsx
@@ -169,7 +169,7 @@ export default function DataTable(props: DataTableProps) {
                     tabIndex={-1}
                     key={row[idProp]}
                     selected={isItemSelected || row[idProp] === highlightItem?.[idProp]}
-                    sx={{ background: (index % 2) ? "#F4F5F9" : "transparent" }}
+                    sx={{ backgroundColor: (index % 2) ? "#F4F5F9" : "transparent !important" }}
                   >
                     {!hideCheckbox &&
                       <TableCell padding="checkbox">
