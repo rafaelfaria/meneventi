39cfeb7a293b44130c0773e786afab88bec3cbce
Rafael Cardoso <rcardoso@atlassian.com>
2023-03-24 10:56:13 +1000

    Implemented the timer countdown

--- src/ui/pages/Timer.tsx
+++ src/ui/pages/Timer.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Page from "./Page";
 
 import { Box, Button, Divider, Grid, Paper, Typography } from '@mui/material';
@@ -6,9 +7,75 @@ import { styled } from '@mui/material/styles';
 import DataList from "../components/table/DataList";
 import TimerData from "../components/TimerData";
 import AppConfig from "../../config";
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import { useTimer } from "react-timer-hook";
 
+type TimerState = 'STOPPED' | 'RUNNING' | 'PAUSED';
 
 export default function Timer() {
+
+  const moveNextRound = () => {
+    console.log('NEXT ROUND');
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+    restart(time, true);
+  }
+
+  const [ timerState, setTimerState ] = useState<TimerState>('STOPPED');
+  const [ round, setRound ] = useState(0);
+
+  const {
+      seconds,
+      minutes,
+      isRunning,
+      start,
+      pause,
+      resume,
+      restart,
+    } = useTimer({ autoStart: true, expiryTimestamp: new Date(), onExpire: moveNextRound });
+
+  useEffect(() => {
+    switch (timerState) {
+      case 'RUNNING':
+        resume();
+        break;
+      case 'PAUSED':
+        pause();
+      break;
+    }
+
+
+  }, [timerState])
+
+  const config = {
+    'STOPPED': {
+      label: 'Start',
+      icon: <PlayCircleFilledWhiteIcon />
+    },
+    'RUNNING': {
+      label: 'Pause',
+      icon: <PauseCircleIcon />
+    },
+    'PAUSED': {
+      label: 'Resume',
+      icon: <PlayCircleFilledWhiteIcon />
+    },
+  }
+
+  const toggleTimerState = () => {
+    switch (timerState) {
+      case 'STOPPED':
+        setTimerState('RUNNING');
+        break;
+      case 'RUNNING':
+        setTimerState('PAUSED');
+        break;
+      case 'PAUSED':
+        setTimerState('RUNNING');
+        break;
+    }
+  }
 
   return (
     <Page>
@@ -20,7 +87,7 @@ export default function Timer() {
               <Round>Round 1</Round>
             </Grid>
             <GridFlex item xs={12} md={5}>
-              <Clock>15:00</Clock>
+              <Clock>{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}</Clock>
             </GridFlex>
             <GridFlex item xs={12} md={2}>
               <Divider orientation="vertical"  />
@@ -30,7 +97,10 @@ export default function Timer() {
               <NextBlinds>Next: 200/400</NextBlinds>
             </GridFlex>
             <GridFlex item xs={12}>
-              <ButtonStart variant="contained" sx={{ width: '100%', mt: 5 }}>Start</ButtonStart>
+              <ButtonStart variant="contained" sx={{ width: '100%', mt: 5 }} startIcon={config[timerState].icon} onClick={toggleTimerState} timerState={timerState}>
+                {config[timerState].label}
+              </ButtonStart>
+
             </GridFlex>
           </Grid>
 
@@ -58,7 +128,7 @@ const Round = styled(Paper)(() => ({
   textAlign: 'center',
   fontSize: '20px',
   padding: 10,
-  background: '#3b4275',
+  background: 'linear-gradient(to right, #464da8 30%, #2675f7 70%)',
 }));
 
 const Clock = styled(Typography)(({ theme }) => ({
@@ -79,13 +149,16 @@ const Blinds = styled(Typography)(({ theme }) => ({
   }
 }));
 
-const NextBlinds = styled(Typography)(({ theme }) => ({
+const NextBlinds = styled(Typography)(() => ({
   fontSize: 26,
   color: '#888686',
 }));
 
-const ButtonStart = styled(Button)(() => ({
-  background: 'linear-gradient(to right, #464da8 30%, #2675f7 70%)',
+type TimerStateProps = {
+  timerState: TimerState;
+}
+const ButtonStart = styled(Button)<TimerStateProps>(({ timerState }) => ({
+  background: (timerState === 'RUNNING') ? 'linear-gradient(to right, #bc5757 30%, #cd2f21 70%)' : 'linear-gradient(to right, #327b81 30%, #129963 70%)',
   color:'#FFF',
   fontSize: 20,
   fontWeight: 'bold',
