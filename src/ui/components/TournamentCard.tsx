e0f88cb7ee1595b8136e84337640dc3ab68abf8e
Rafael Cardoso <rcardoso@atlassian.com>
2023-10-13 08:38:35 +1000

    Added number of players

--- src/ui/components/TournamentCard.tsx
+++ src/ui/components/TournamentCard.tsx
@@ -13,7 +13,7 @@ type Props = {
 }
 
 export default function TournamentCard({ data }: Props) {
-
+console.log({ data })
   const { authUser } = useAuth();
   const navigate = useNavigate();
 
@@ -51,7 +51,7 @@ export default function TournamentCard({ data }: Props) {
         <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 10 }}>
           <CardContent sx={{ flex: '1 0 auto' }}>
             <Typography component="div" variant="h5" sx={{ fontSize: 20 }}>
-              {data.name}
+              {data.name} <Typography component="small" sx={{ color: '#CCC' }}>({data.leaderboard?.length} players)</Typography>
             </Typography>
             <Typography variant="subtitle1" color="text.secondary" component="div">
               {data.leaderboard?.filter((item:any) => item.prize && item.prize > 0).map((item: any) => `${(item.place === 1) ? '👑' : '🏆'} ${item.name}`).join(', ')}
